160da75f45f713b3f759bb4f201a1c04440e5c64
DESKTOP-AAHK534\davii <daviita111@gmail.com>
2022-07-17 10:55:32 -0300

    new pakage

---
+++ Assets/Scenes/MainMenu.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void SettingsMenu()
+    {
+        SceneManager.LoadScene("MenuConfig");
+    }
+	
+    public void Main()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
